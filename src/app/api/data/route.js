97077422b9bd027e1b409b94ae29c319af956c67
Rohit <rkrohitkumar893@gmail.com>
2026-01-29 00:02:09 +0530

    fix: Get method

--- src/app/api/data/route.js
+++ src/app/api/data/route.js
@@ -1,13 +1,13 @@
 import { NextResponse } from 'next/server';
 import newPool from '../../../dbConfig/db.js'
 
-export async function GET(request) {
+export async function GET() {
   try {
     const query = "select * from apps_list_for_ca_dashboard"
     const result = await newPool.query(query)
     return NextResponse.json(result.rows);
    
   } catch (error) {
-    return NextResponse.json({ error: 'Failed to fetch' }, { status: 500 });
+    return NextResponse.json({ success:false, message: 'Failed to fetch' }, { status: 500 });
   }
 }
